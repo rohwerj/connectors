f1f8d955318f5c8c73c500a9dc529d6e4aff5b7d
juanfparrado <juanfparrado@igg.com.co>
2023-10-13 15:12:40 -0500

    ya esta funcional

--- docker.sh
+++ docker.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 docker run --rm --name=connectors -v $PWD/target/openweather-api-0.1.0-SNAPSHOT.jar:/opt/app/connector.jar --env-file env.txt camunda/connectors:0.2.0
+image: docker run --rm --name=connectors -v "C:/Users/Admin/Desktop/xample/camunda-8-connector-openweather-api/target/openweather-api-0.1.0-SNAPSHOT.jar":/opt/app/connector.jar --env-file env.txt camunda/connectors-bundle:0.23.2
+image: docker run --rm --name=connectors -v "C:/Users/Admin/Desktop/xample/camunda-8-connector-openweather-api/target/openweather-api-0.1.0-SNAPSHOT.jar":/opt/app/connector.jar --env-file env.txt camunda/connectors-bundle:0.2.0
