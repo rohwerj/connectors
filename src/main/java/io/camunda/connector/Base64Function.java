81fee3ca24fc306ace688d0737bf1bf1dbbf5549
juanfparrado <juanfparrado@igg.com.co>
2023-10-30 10:23:11 -0500

    FUNCIONA se añade cmis servicein its own class

--- src/main/java/io/camunda/connector/Base64Function.java
+++ src/main/java/io/camunda/connector/Base64Function.java
@@ -1,24 +1,39 @@
 package io.camunda.connector;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.lang.reflect.Field;
 import java.math.BigInteger;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
+import java.util.UUID;
 
 import org.apache.chemistry.opencmis.client.api.CmisObject;
 import org.apache.chemistry.opencmis.client.api.Document;
 import org.apache.chemistry.opencmis.client.api.FileableCmisObject;
 import org.apache.chemistry.opencmis.client.api.Folder;
+import org.apache.chemistry.opencmis.client.api.ItemIterable;
+import org.apache.chemistry.opencmis.client.api.ObjectId;
+import org.apache.chemistry.opencmis.client.api.ObjectType;
+import org.apache.chemistry.opencmis.client.api.OperationContext;
+import org.apache.chemistry.opencmis.client.api.QueryResult;
+import org.apache.chemistry.opencmis.client.api.Relationship;
+import org.apache.chemistry.opencmis.client.api.Repository;
 import org.apache.chemistry.opencmis.client.api.Session;
 import org.apache.chemistry.opencmis.client.api.SessionFactory;
 import org.apache.chemistry.opencmis.client.api.Tree;
@@ -26,10 +41,23 @@ import org.apache.chemistry.opencmis.client.runtime.SessionFactoryImpl;
 import org.apache.chemistry.opencmis.commons.PropertyIds;
 import org.apache.chemistry.opencmis.commons.SessionParameter;
 import org.apache.chemistry.opencmis.commons.data.ContentStream;
+import org.apache.chemistry.opencmis.commons.enums.BaseTypeId;
 import org.apache.chemistry.opencmis.commons.enums.BindingType;
+import org.apache.chemistry.opencmis.commons.enums.RelationshipDirection;
 import org.apache.chemistry.opencmis.commons.enums.VersioningState;
 import org.apache.chemistry.opencmis.commons.exceptions.CmisRuntimeException;
 import org.apache.chemistry.opencmis.commons.impl.dataobjects.ContentStreamImpl;
+import org.apache.chemistry.opencmis.commons.impl.json.JSONObject;
+import org.apache.http.HttpResponse;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
 import org.apache.tika.exception.TikaException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -40,7 +68,12 @@ import org.xml.sax.SAXException;
 
 import com.co.igg.catastro.common.models.DocumentoProceso;
 import com.co.igg.catastro.common.models.Proceso;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import io.camunda.connector.api.annotation.OutboundConnector;
@@ -49,6 +82,7 @@ import io.camunda.connector.api.outbound.OutboundConnectorFunction;
 import io.camunda.dao.IDocumentProcessDao;
 import io.camunda.interfaces.IProcesoService;
 import io.camunda.models.CustomMultipartFile;
+import io.camunda.cmis.CmisService;
 import jakarta.annotation.PostConstruct;
 
 @OutboundConnector(
@@ -56,30 +90,8 @@ import jakarta.annotation.PostConstruct;
 public class Base64Function implements OutboundConnectorFunction {
 
   private Session session;
-
-
+  private CmisService cmisService;
   private static final Logger LOGGER = LoggerFactory.getLogger(Base64Function.class);
-
-  @PostConstruct
-  public void init()
-  {
-
-      String alfrescoBrowserUrl = System.getenv("alfresco.repository.url") + "/api/-default-/public/cmis/versions/1.1/browser";
-
-      Map<String, String> parameter = new HashMap<String, String>();
-
-    /*   parameter.put(SessionParameter.USER, System.getenv("alfresco.repository.user"));
-      parameter.put(SessionParameter.PASSWORD, System.getenv("alfresco.repository.pass"));
-      parameter.put(SessionParameter.BROWSER_URL, alfrescoBrowserUrl); */
-
-      parameter.put(SessionParameter.USER, "admin");
-      parameter.put(SessionParameter.PASSWORD, "admin");
-      parameter.put(SessionParameter.BROWSER_URL, "http://192.168.6.33:8080/alfresco/api/-default-/public/cmis/versions/1.1/browser");
-      parameter.put(SessionParameter.BINDING_TYPE, BindingType.BROWSER.value());
-      SessionFactory factory = SessionFactoryImpl.newInstance();
-      session = factory.getRepositories(parameter).get(0).createSession();
-
-  }
 
   @Override
   public Object execute(OutboundConnectorContext context) throws Exception {
@@ -99,22 +111,29 @@ public class Base64Function implements OutboundConnectorFunction {
       byte[][] files= connectorRequest.getFiles();
       List<MultipartFile> multipartFiles = convertBytesToMultipartFiles(files, filesNames);
 
-      if (null != multipartFiles && multipartFiles.size() > 0) 
-				        {
-				            for (MultipartFile multipartFile : multipartFiles) {
-				                String fileName = multipartFile.getOriginalFilename();
-LOGGER.info("filename: {}",fileName);
-LOGGER.info("inputstream: {}",multipartFile.getInputStream());
-								}
-				            }
-
-				        
+      if (null != multipartFiles && multipartFiles.size() > 0) {
+    	    for (MultipartFile multipartFile : multipartFiles) {
+    	        String fileName = multipartFile.getOriginalFilename();
+    	        LOGGER.info("filename: {}", fileName);
+    	        LOGGER.info("inputstream: {}", multipartFile.getInputStream());
+//			      try {
+//			        Document docCreated=cmisService.uploadDocumentToAlfresco( fileName,  multipartFile, Long.parseLong(respuesta) );
+//			        if(docCreated==null){
+//			          throw new FileExistsException("El archivo ya existe en la base de datos.");
+//			        }
+//			//											String docInB64=procesarDocumentos.getInformationFromFile(docCreated);
+//			      } catch (NumberFormatException | IOException | SAXException | TikaException e) {
+//			        // TODO Auto-generated catch block
+//			        e.printStackTrace();
+//			      }
+    	    }
+    	}
     var result = new Base64Result();
     LOGGER.info("getting out of connector");
     return result;
   }
 
-  
+ 
     private List<MultipartFile> convertBytesToMultipartFiles(byte[][] fileBytesArray, String[] fileNamesArray) {
         List<MultipartFile> recreatedFiles = new ArrayList<>();
         for (String fileName : fileNamesArray) {
---
+++ src/main/java/io/camunda/services/ICmisService.java
@@ -0,0 +1,13 @@
+package io.camunda.services;
+
+import java.io.IOException;
+
+import org.apache.chemistry.opencmis.client.api.Document;
+import org.apache.chemistry.opencmis.client.api.Folder;
+import org.apache.tika.exception.TikaException;
+import org.springframework.web.multipart.MultipartFile;
+import org.xml.sax.SAXException;
+
+public interface ICmisService {
+	public Folder getDocLibFolder( String siteName, String folder);
+}
